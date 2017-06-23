e8ad1d4756830a3bd76872126657c37ea8794c18
Bruno <bruno.brandao.fosse@gmail.com>
2017-06-22 23:59:27 -0300

    Começar vídeo 19 - Melhorar a Listagem dos Itens

--- resources/views/painel/templates/template.blade.php
+++ resources/views/painel/templates/template.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{{$titulo or 'Painel Curso'}}</title>
         <link rel="shortcut icon" href="">
-        <link rel="stylesheet" type="text/css" href="">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
         <link href="https://fonts.googleapis.com/css?family=Mogra" rel="stylesheet">
         
         <style>
@@ -22,5 +22,6 @@
         
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="{{ asset('js/bootstrap.js') }}"></script>
     </body>
 </html>
